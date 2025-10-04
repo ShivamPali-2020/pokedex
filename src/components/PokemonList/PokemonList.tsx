20832c9c4649cd44474e020e9c34e418e4c7783a
Shivam Pali <pali.shivam2020@gmail.com>
2025-10-04 16:19:56 +0530

    fix: browser back button history issue

--- src/components/PokemonList/PokemonList.tsx
+++ src/components/PokemonList/PokemonList.tsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import { createUseStyles } from 'react-jss';
-import { Pokemon } from 'hooks/useGetPokemons';
+import { Pokemon } from 'types/Pokemon';
 import PokemonListItem from './PokemonListItem';
 import { useNavigate, useSearchParams } from 'react-router-dom';
 import PokemonDetailsModal from './PokemonDetailsModal';
@@ -22,17 +22,21 @@ export const PokemonList = ({
   }, [searchParams]);
 
   const handlePokemonClick = (pkmn: Pokemon) => {
-    navigate(`?id=${pkmn.id}&name=${pkmn.name}`, { replace: true });
+    const params = new URLSearchParams();
+    params.set('id', pkmn.id.toString());
+    params.set('name', pkmn.name);
+    setSearchParams(params, { replace: true });
+    navigate({ search: params.toString() }, { replace: true });
     setOpenModal(true);
   };
 
   const handleModalClose = () => {
     setOpenModal(false);
-    setSearchParams(new URLSearchParams());
+    setSearchParams(new URLSearchParams(), { replace: true });
   };
 
   return (
-    <div className={classes.root}>
+    <>
       <div className={classes.pokemonList}>
         {pokemons.map((pkmn: Pokemon) => (
           <PokemonListItem
@@ -48,25 +52,22 @@ export const PokemonList = ({
       {openModal && (
         <PokemonDetailsModal open={openModal} onClose={handleModalClose} />
       )}
-    </div>
+    </>
   );
 };
 
-const useStyles = createUseStyles(
-  {
-    root: {
-      width: '100%',
-      textAlign: 'center',
-      boxSizing: 'border-box',
-    },
-    pokemonList: {
-      display: 'grid',
-      gridTemplateColumns: 'repeat(auto-fill, minmax(150px, 1fr))',
-      gap: '10px',
-      flexWrap: 'wrap',
-      justifyContent: 'center',
-      alignItems: 'center',
-    },
+const useStyles = createUseStyles({
+  root: {
+    width: '100%',
+    textAlign: 'center',
+    boxSizing: 'border-box',
   },
-  { name: 'PokemonList' }
-);
+  pokemonList: {
+    display: 'grid',
+    gridTemplateColumns: 'repeat(auto-fill, minmax(150px, 1fr))',
+    gap: '10px',
+    flexWrap: 'wrap',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+});
